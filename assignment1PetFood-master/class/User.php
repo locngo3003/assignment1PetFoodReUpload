e6d8a542e3856a110b29bfd976c3ea2455c99e7e
locngo3003 <locnxse04549@gmail.com>
2020-06-09 08:56:25 +1200

    case study 2

--- assignment1PetFood-master/class/User.php
+++ assignment1PetFood-master/class/User.php
@@ -3,5 +3,24 @@
 
 class User
 {
+public $id;
+public $username;
+private $password;
+public $name;
+
+    /**
+     * User constructor.
+     * @param $id
+     * @param $username
+     * @param $password
+     * @param $name
+     */
+    public function __construct($id, $username, $password, $name)
+    {
+        $this->id = $id;
+        $this->username = $username;
+        $this->password = $password;
+        $this->name = $name;
+    }
 
 }
