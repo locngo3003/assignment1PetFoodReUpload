c9e58c51507bd9bc857102353e165e4f6f788580
locngo3003 <locnxse04549@gmail.com>
2020-06-09 09:29:34 +1200

    case study 2

--- assignment1PetFood-master/class/User.php
+++ assignment1PetFood-master/class/User.php
@@ -7,12 +7,13 @@
  */
 include_once "DB.php";
 include_once "Category.php";
+include_once  "Product.php";
 class User
 {
-public $id;
-public $username;
-private $password;
-public $name;
+    public $id;
+    public $username;
+    private $password;
+    public $name;
 
     /**
      * User constructor.
@@ -28,19 +29,40 @@ public $name;
         $this->password = $password;
         $this->name = $name;
     }
-    public function viewCategories(){
-       $conn = (new DB())->connection;// create connection from class
-       $sql = "select * from Category";
-       $categories = array();
-       $result = $conn ->query($sql);
-       if ($result->num_rows >0){
-           while ($row = $result ->fetch_assoc()){
-               $category = new category(["id"],$row["name"],$row["picture"]);
-               array_push($categories,$category);
-           }
-       }
-       $conn->close();
+
+    /**
+     * @name viewCategories
+     * @return category array
+     */
+    public function viewCategories()
+    {
+        $conn = (new DB())->connection;// create connection from class
+        $sql = "select * from Category"; //my query
+        $categories = array(); // my categories in an array
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $category = new category(["id"], $row["name"], $row["picture"]);
+                array_push($categories, $category);
+            }
+        }
+        $conn->close();
         return $categories;
     }
 
+    public function showProductsByCategory($categoryId){
+        $conn = (new DB())->connection;
+        $sql = "select * from Product where categoryid=".$categoryId; //. means merge two string
+        $products = array();
+        $result = $conn->query($sql);
+        if($result->num_rows>0){
+            while ($row = $result->fetch_assoc()){
+                $product = new Product($row["id"],$row["name"],$row["price"],$row["description"],$row["categoryId"]);
+                array_push($products, $product);
+            }
+        }
+
+        $conn->close();
+        return $products;
+    }
 }
